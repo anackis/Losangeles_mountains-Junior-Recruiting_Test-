b8e7ead60f488eb3bb937b3208a738b1626069dc
Alex <rengarsanj@gmail.com>
2023-06-05 12:05:23 +0300

    1 day project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useCallback  } from "react";
+
+import NavBar from './components/nav-bar/nav-bar';
+import Main from "./pages/main/main";
+import History from "./pages/history/history";
+import Team from "./pages/team/team";
+
+import './App.scss';
+
 
 function App() {
+  const [activeLink, setActiveLink] = useState("Main");
+
+  const handleNavItemClick = useCallback((linkName) => {
+    setActiveLink(linkName);
+  }, []);
+
+
+  let activeComponent;
+  if (activeLink === 'Main') {
+    activeComponent = <Main />
+  } else if (activeLink === 'History') { 
+    activeComponent = <History/>;
+  } else if (activeLink === 'Team') {
+    activeComponent = <Team 
+      handleNavItemClick={handleNavItemClick} 
+      activeLink={activeLink}
+    />;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar handleNavItemClick={handleNavItemClick} activeLink={activeLink}/>
+      <div className="main__wrapper">
+        {activeComponent}
+      </div>
     </div>
   );
 }
